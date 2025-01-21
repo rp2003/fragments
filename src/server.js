994d514addc3fb4c73f17c1fd10871e9cd02273a
rp2003 <riya2@myseneca.ca>
2025-01-20 20:13:38 -0500

    Switched to src/index.js as main entry point

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 // src/server.js
 
+require('dotenv').config();
+
 // We want to gracefully shutdown our server
 const stoppable = require('stoppable');
 
@@ -10,7 +12,7 @@ const logger = require('./logger');
 const app = require('./app');
 
 // Get the desired port from the process' environment. Default to `8080`
-const port = parseInt(process.env.PORT || '8080', 10);
+const port = parseInt(process.env.PORT || 8080, 10);
 
 // Start a server listening on this port
 const server = stoppable(
